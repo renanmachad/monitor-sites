f3737676097413f6942df318274766dc6f50d465
renanmachad <renanmachadopinho@gmail.com>
2022-10-16 16:34:04 -0300

    adicionando novas funcionalidades

--- src/main.go
+++ src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -18,11 +24,14 @@ func main() {
 		case 1:
 			initMonitoring()
 		case 2:
-			fmt.Println("Adicione ou remova os valores")
+			fmt.Println("Adicione sites à lista de monitoramento")
+			addSites()
 			os.Exit(0)
 		case 3:
 			fmt.Println("Encerrando programa")
 			os.Exit(0)
+		case 4:
+			ReadLog()
 		default:
 			fmt.Print("Comando inválido")
 			os.Exit(0)
@@ -35,8 +44,9 @@ func Options() {
 	fmt.Println("Escolha a opção desejada abaixo")
 
 	fmt.Println("1- Iniciar monitoramento")
-	fmt.Println("2- Modificar sites monitorados")
+	fmt.Println("2- Adicionar sites monitorados")
 	fmt.Println("3- Fechar programa")
+	fmt.Println("4- Exibir logs")
 }
 
 // just print the introduction
@@ -55,24 +65,101 @@ func InputComand() int {
 	return comand
 }
 
+// restante do código omitido
+
+func testaSite(site string) {
+
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		WriteLog(site, true)
+
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		WriteLog(site, false)
+	}
+}
+
 func initMonitoring() {
 	fmt.Println("Iniciando monitoramento")
 
 	// slice of sites to monitoring
-	sites := []string{"https://wavetech-st.com", "https://dynamox.net.com", "https://random-status-code.herokuapp.com/"}
+	sites := readFile()
 
 	// map the slice of websites
 	for i, site := range sites {
-		fmt.Println(i)
-		resp, error := http.Get(site)
+		fmt.Println("Verificando site:", "- ", i, site)
+		testaSite(site)
+	}
 
-		if resp.StatusCode == 200 {
-			fmt.Println("Site ativo")
-		} else {
-			fmt.Println("Site com problemas:", resp.StatusCode)
+}
+
+func readFile() []string {
+	var sites []string
+	file, err := os.Open("./config/sites.txt")
+
+	if err != nil {
+		fmt.Println("Um erro ocorreu", err)
+
+	}
+	reader := bufio.NewReader(file)
+
+	for {
+
+		line, error := reader.ReadString('\n')
+
+		line = strings.TrimSpace(line)
+
+		sites = append(sites, line)
+		if error == io.EOF {
+			break
 		}
 
-		fmt.Print(error)
 	}
+	file.Close()
+	return sites
+}
+
+func WriteLog(site string, status bool) {
+	// func to register status in a log file
+	arquivo, err := os.OpenFile("./config/logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05 ") + site + " --online: " + strconv.FormatBool(status) + "\n")
+
+	if err != nil {
+		fmt.Println("Erro ao escrever arquivo", err)
+	}
+
+	arquivo.Close()
+}
+
+func ReadLog() {
+	file, err := ioutil.ReadFile("./config/logs.txt")
+
+	fmt.Println(string(file))
+
+	if err != nil {
+		fmt.Println("Erro ao ler arquivo de log: ", err)
+	}
+
+}
+
+func addSites() {
+	var new_site string
+	file, err := os.Open("./config/config.txt")
+
+	fmt.Scan(&new_site)
+	file.WriteString(new_site)
+
+	if err != nil {
+		fmt.Println("Erro ao ler ou escrever dados no arquivo")
+	}
+	file.Close()
 
 }
